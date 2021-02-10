f2d390fb6c5748de560ebc36e4435adf4d89cc5a
Anthony Pineau <anthonypineau85@gmail.com>
2021-02-10 15:52:04 +0200

    First commit

--- src/akmst/Akmst.java
+++ src/akmst/Akmst.java
@@ -15,7 +15,7 @@ public class Akmst {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Bienvenue sur l'application Akmst");
     }
     
 }
