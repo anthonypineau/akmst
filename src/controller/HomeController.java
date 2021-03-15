4f312af2bb1bb7480ba90239c698ea38f566b8fc
hugo landre <hugolandre7@gmail.com>
2021-03-15 17:45:53 +0200

    homeview graphics

--- src/controller/HomeController.java
+++ src/controller/HomeController.java
@@ -5,6 +5,15 @@
  */
 package controller;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.CategoryPlot;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.data.general.DefaultPieDataset;
 import view.HomeView;
 
 /**
@@ -23,5 +32,56 @@ public class HomeController {
     
     public void displayHomeView(){
         this.homeView.setVisible(true);
+        this.faitUnGraphLa();
     }    
+    
+     public void faitUnGraphLa(){
+        DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+        dataset.setValue(1,"","premier");
+        dataset.setValue(7,"","deuxième");
+        dataset.setValue(5,"","troisième");
+        dataset.setValue(2,"","quatrième");
+        
+        JFreeChart chart = ChartFactory.createBarChart("","", "", dataset, PlotOrientation.HORIZONTAL, false, false, false);
+        CategoryPlot catPlot =chart.getCategoryPlot();
+        catPlot.setRangeGridlinePaint(Color.BLACK);
+        
+        ChartPanel chartPanel = new ChartPanel(chart);
+        this.homeView.getJPanel1().removeAll();
+        this.homeView.getJPanel1().add(chartPanel, BorderLayout.CENTER);
+        this.homeView.getJPanel1().validate();
+    }
+    
+        public void faitUnGraphPie(){
+            
+        DefaultPieDataset dataset = new DefaultPieDataset();
+        dataset.setValue("premier", 1);
+        dataset.setValue("deuxième", 2);
+        dataset.setValue("troisième", 7);
+        dataset.setValue("quatrième", 3);
+        
+        JFreeChart chart = ChartFactory.createPieChart("pie chart", dataset, true, true, false);
+        
+        ChartPanel chartPanel = new ChartPanel(chart);
+        this.homeView.getJPanel1().removeAll();
+        this.homeView.getJPanel1().add(chartPanel, BorderLayout.CENTER);
+        this.homeView.getJPanel1().validate();
+    }
+        
+    public void faitUnGraphLine(){
+        DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+        dataset.setValue(1,"","premier");
+        dataset.setValue(7,"","deuxième");
+        dataset.setValue(5,"","troisième");
+        dataset.setValue(2,"","quatrième");
+        
+        JFreeChart chart = ChartFactory.createLineChart("","", "", dataset, PlotOrientation.VERTICAL, false, false, false);
+        CategoryPlot catPlot =chart.getCategoryPlot();
+        catPlot.setRangeGridlinePaint(Color.BLACK);
+        
+        ChartPanel chartPanel = new ChartPanel(chart);
+        this.homeView.getJPanel1().removeAll();
+        this.homeView.getJPanel1().add(chartPanel, BorderLayout.CENTER);
+        this.homeView.getJPanel1().validate();
+    }
 }
